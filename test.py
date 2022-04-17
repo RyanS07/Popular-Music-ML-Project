e5d51efe17c5b63e81e935258c16eeb375b9dbae
RyanS07 <56851349+RyanS07@users.noreply.github.com>
2022-04-16 20:24:08 -0400

    all 9 assignments

---
+++ test.py
@@ -0,0 +1,12 @@
+import json
+
+items = json.loads('[{"id": 1, "text": "Item 1"}, {"id": 2, "text": "Item 2"}]')
+
+for item in items:
+    print(item['text'])
+
+
+
+def hello(greeting, name):
+
+    return greeting + name
